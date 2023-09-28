866df39296475242ac5663d54f495f66913dbac0
Dron Kishor <93179377+dronacharya27@users.noreply.github.com>
2023-09-29 00:55:02 +0530

    allowed host, buid.sh changes

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,3 @@
 # build_files.sh
 pip install -r requirements.txt
-<<<<<<< HEAD
 python3.9 manage.py collectstatic
-=======
-python3.9 manage.py collectstatic
->>>>>>> 39c3a03cb5106d273a4a87c4de094581750cb386
